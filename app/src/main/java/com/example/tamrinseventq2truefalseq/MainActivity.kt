6d02c4d9feb03ff3eec11f22afb566fbcd9e4091
Samin <bmst555m@gmail.com>
2022-02-25 13:19:07 +0330

    set on click function

--- app/src/main/java/com/example/tamrinseventq2truefalseq/MainActivity.kt
+++ app/src/main/java/com/example/tamrinseventq2truefalseq/MainActivity.kt
@@ -1,16 +1,70 @@
 package com.example.tamrinseventq2truefalseq
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.tamrinseventq2truefalseq.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     var listOfQuestion=Questioner.qList
+    var qNumber=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.textViewQ.text=listOfQuestion[qNumber].question
     }
 
+    private fun initView(){
+       // updateQuestion(listOfQuestion[qNumber])
+        val intent= Intent(this,ChedActivity2::class.java)
+        intent.putExtra("question",listOfQuestion[qNumber].answer)
+        startActivity(intent)
+    }
+    private fun updateQuestion(question: Question) {
+          binding.textViewQ.text=question.question
+    }
+    fun choseButton(view:View){
+        if (view==binding.buttonPrev){
+            if (qNumber==0){
+                view.isEnabled
+            }else{
+                qNumber--
+                updateQuestion(listOfQuestion[qNumber])
+            }
+        }
+        if (view==binding.buttonNext){
+            if (qNumber==9){
+                view.isEnabled
+            }else{
+                qNumber++
+                updateQuestion(listOfQuestion[qNumber])
+            }
+        }
+        if (view==binding.buttonCheat){
+            initView()
+            listOfQuestion[qNumber].cheat=true
+        }
+        if (view==binding.buttonF){
+            if (binding.buttonF.text==listOfQuestion[qNumber].answer.toString()){
+                Toast.makeText(this,"Correct!",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"Incorrect!",Toast.LENGTH_SHORT).show()
+            }
+            view.isEnabled
+            binding.buttonT.isEnabled
+        }
+        if (view==binding.buttonT){
+            if (binding.buttonT.text==listOfQuestion[qNumber].answer.toString()){
+                Toast.makeText(this,"Correct!",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"Incorrect!",Toast.LENGTH_SHORT).show()
+            }
+            view.isEnabled
+            binding.buttonF.isEnabled
+        }
+    }
 }
--- app/src/main/java/com/example/tamrinseventq2truefalseq/Question.kt
+++ app/src/main/java/com/example/tamrinseventq2truefalseq/Question.kt
@@ -3,18 +3,18 @@ package com.example.tamrinseventq2truefalseq
 data class Question(var question: String, var answer: Boolean, var cheat: Boolean = false)
 object Questioner {
     var qList = mutableListOf<Question>()
-    private var q1 = Question("Each natural number has at least two natural counters", false)
-    private var q2 = Question("From one point just a straight line goes on.", false)
-    private var q3 = Question("B M. two different prime numbers is one", true)
-    private var q4 = Question("The square is a formal form", true)
-    private var q5 =
+   var q1 = Question("Each natural number has at least two natural counters", false)
+     var q2 = Question("From one point just a straight line goes on.", false)
+    var q3 = Question("B M. two different prime numbers is one", true)
+   var q4 = Question("The square is a formal form", true)
+     var q5 =
         Question("TThe result of multiplication a negative number in a negative number is a positive number", true)
-    private var q6 =
+   var q6 =
         Question("The result of the sum of the number with its symmetrical is zero", true)
-    private var q7 = Question("A negative numerical product is negative in a positive number", true)
-    private var q8 = Question("Water is not from the combination of oxygen and hydrogen", false)
-    private var q9 = Question(" Any positive integer is larger than any integer negative", true)
-    private var q10 = Question("قرینه ی قرینه ی هر عدد با خودش برابر است", true)
+  var q7 = Question("A negative numerical product is negative in a positive number", true)
+     var q8 = Question("Water is not from the combination of oxygen and hydrogen", false)
+    var q9 = Question(" Any positive integer is larger than any integer negative", true)
+    var q10 = Question("قرینه ی قرینه ی هر عدد با خودش برابر است", true)
     init {
         add()
     }
